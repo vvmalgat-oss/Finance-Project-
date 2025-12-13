d49eeb87cc97378b01af2e7b3b421724b89c12d2
vvmalgat-oss <vvmalgat@mtu.edu>
2025-12-14 10:27:45 +1100

    Delete Sector Count.sql

--- Sector Count.sql
+++
@@ -1,9 +0,0 @@
--- Analysing dataset by sectors and categorising companies where the price is 0 or less than 0.
-
-SELECT  
-Sector,
-COUNT(*) AS n,
-COUNTIF(price IS NULL OR price <= 0) AS bad_price_rows
-FROM `database-sp500dataanalysis.sp500dataanalysis.sp500data`
-GROUP BY sector
-ORDER BY n DESC;
